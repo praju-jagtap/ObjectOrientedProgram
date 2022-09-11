24c4b4efa2639b8d3ea47d5e1aaee38e9717ecb5
praju-jagtap <prajaktajagtap2412@gmail.com>
2022-09-11 16:07:28 +0530

    [Prajakta] Add: Display Welcome Message

---
+++ OOP Problems/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OOP_Problems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Object Oriented Program");
+        }
+    }
+}
